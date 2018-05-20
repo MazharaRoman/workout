e6a791589366a96cf82d5ee29feaca1fa9b0b6ab
Roman Mazhara <mazhararom@gmail.com>
2018-05-20 22:28:11 +0300

    moving forward. side menu, styling via unique

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import HeadBar from '../UI/HeadBar/HeadBar';
+import classes from './Layout.css';
 
 export default props => (
-    <div className="Screen">
+    <div className={classes.Layout}>
+    <div className={classes.SideMenu}> </div>
         <HeadBar />
-        <main className="Main">{props.children}</main>
-        <footer>footer</footer>
+        <main className={classes.Main}>{props.children}</main>
+        {/* <footer>footer</footer> */}
     </div>
 );
--- src/components/UI/HeadBar/HeadBar.js
+++ src/components/UI/HeadBar/HeadBar.js
@@ -3,21 +3,27 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import classes from './HeadBar.css';
+
+
 /**
  * A simple example of `AppBar` with an icon on the right.
  * By default, the left icon is a navigation-menu.
  */
 const HeadBar = () => (
-    <div className="Header">
-        <AppBar position="static" color="default">
-            <Toolbar>
+        <AppBar position="static" color="default" > 
+            <Toolbar className={classes.Header}>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
                 <Typography variant="title" color="inherit">
                     OOOSHEEE!
                 </Typography>
                 <Button color="inherit">Login</Button>
             </Toolbar>
         </AppBar>
-    </div>
 );
 
 export default HeadBar;
